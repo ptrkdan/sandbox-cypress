9c4a64d1ef31716484c746f2fa3ba8c4ce689a51
Patrick Dan Lacuna <dan.lacuna@monstar-lab.com>
2024-03-25 11:33:17 +0900

    Change reporter to add screenshots in report

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'cypress';
+import cypressMochawesomeReporterPlugin from 'cypress-mochawesome-reporter/plugin';
 
 export default defineConfig({
 	projectId: '29mpmo',
 	watchForFileChanges: false,
-	reporter: 'mochawesome',
+	reporter: 'cypress-mochawesome-reporter',
 	reporterOptions: {
-		reportDir: 'cypress/results',
-		overwrite: false,
+    reportDir: 'cypress/results',
+    inlineAssets: true,
+    embedScreenshots: true,
+	},
+	screenshotsFolder: 'cypress/results/assets',
+	retries: {
+		runMode: 1,
+		openMode: 0
 	},
 	e2e: {
 		// baseUrl: 'https://engineering.monstar-lab.com/en/',
@@ -16,6 +23,7 @@ export default defineConfig({
 		},
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
+			cypressMochawesomeReporterPlugin(on);
 		},
 		excludeSpecPattern: ['**/cypress/e2e/blog.cy.ts']
 	}
